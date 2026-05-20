6182e72d4d9fda450c990ee2042c75e2a077f544
kanhaiya-ML <personalkanhaiya2008@gmail.com>
2026-05-20 08:25:45 +0530

    made some changes

--- Fastapi.py
+++ Fastapi.py
@@ -60,10 +60,19 @@ def ask(request:Request):
         return {"answer": result["output"]}
     
 if __name__ == "__main__":
+
     import uvicorn
-    import os
+
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+
+    print(f"Starting on port: {port}")
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=port,
+        reload=False
+    )
 
 
 
